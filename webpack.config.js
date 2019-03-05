c932911657e0a7beb9c03f604eaafa7e954d51ba
Colt Steele <colt@steele.com>
2019-03-05 15:24:55 -0800

    Cache busting and HtmlWebpackPlugin
    - Configured webpack to use contentHash in bundle file name
    - This caused a problem with out script tag in index.html
    - We installed HtmlWebpackPlugin to help us generate a new index.html
    - It automatically includes the correct script tag at the bottom
    - We created a template file that we passed in called template.html
    - We deleted the original index.html because we don't need it anymore
    - Make sure you are opening dist/index.html now to view the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
