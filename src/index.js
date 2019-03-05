65c2ac47091e301b3adc5b35a68c1870b16eafb7
Colt Steele <colt@steele.com>
2019-03-05 14:50:31 -0800

    Add first loaders to handle css
    - Installed style-loader and css-loader
    - Configured webpack.config to use both loaders on css files
    - Remember the order we use them in webpack.config matters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { run } from "./app/app";
+import "./main.css";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
