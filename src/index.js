4d87613b5a66d645a2ec5c6ff4183c3a20298795
Phong NHH <phongnguyenhh.1996@gmail.com>
2021-04-12 20:47:31 +0700

    init source

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import { run } from "./app/app";
 import "./main.scss";
-import { AlertService } from "./app/alert.service";
-import { ComponentService } from "./app/component.service";
-const alertService = new AlertService();
-const componentService = new ComponentService();
-run(alertService, componentService);
-console.log("J");
